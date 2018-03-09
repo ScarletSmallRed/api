47629ecfed2c2ccb7ba6e21b46c5f92f408129f3
wawa\lee <fadetime@outlook.com>
2018-03-09 20:11:27 +0800

    lee

--- models/area.js
+++ models/area.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const areasSchema = mongoose.Schema({
     areaName: { type: String },
     areaTime: { type: String },
-    areaDescription: { type: String }
+    areaDescription: { type: String },
+    areaStartTime:[{type: String}]
 });
 
 module.exports = mongoose.model('area', areasSchema);
--- models/good.js
+++ models/good.js
@@ -4,8 +4,10 @@ const goodsSchema = mongoose.Schema({
   goodsName: { type: String },
   goodsSize: { type: String },
   goodsPrice: { type: Number },
-  goodsImage: { type: String },
-  goodsCategory: {type: String, require: true}
+  goodsImage: { type: String ,default: 'uploads\\image-1520326629725.jpg'},
+  goodsCategory: {type: String, require: true},
+  goodsDescription:{type: String,default: '这里写小红要求的描述！！！'},
+  goodsSold:{type:Number,default: 0}
 });
 
 module.exports = mongoose.model('goods', goodsSchema);
