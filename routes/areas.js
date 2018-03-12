2cc2043f5024f93fe2e1862e4495c87314da39c3
fadetime <fadetime@163.com>
2018-03-12 16:13:05 +0800

    v2

--- routes/areas.js
+++ routes/areas.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const AreaController = require('../controllers/areas')
 const checkAuth = require('../middleware/check-auth');
 
-router.get('/', checkAuth,AreaController.areas_get_all);
+router.get('/', AreaController.areas_get_all);
 router.get('/:areaId', checkAuth, AreaController.areas_get_area);
 router.post('/create', checkAuth, AreaController.areas_create_area);
 router.patch('/:areaId', checkAuth, AreaController.areas_update_area);
