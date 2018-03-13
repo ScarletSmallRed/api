463d1e6f534159f609e969dfb44d929ff6129d0d
qiang001 <35440329+qiang001@users.noreply.github.com>
2018-03-13 14:28:00 +0800

    ...

--- routes/areas.js
+++ routes/areas.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const AreaController = require('../controllers/areas')
+const express = require('express');
+const router = express.Router();
+const AreaController = require('../controllers/areas');
 const checkAuth = require('../middleware/check-auth');
 
-<<<<<<< HEAD
-router.get('/',AreaController.areas_get_all);
-=======
 router.get('/', AreaController.areas_get_all);
->>>>>>> leev2
 router.get('/:areaId', checkAuth, AreaController.areas_get_area);
 router.post('/create', checkAuth, AreaController.areas_create_area);
 router.patch('/:areaId', checkAuth, AreaController.areas_update_area);
 router.delete('/:areaId', checkAuth, AreaController.areas_delete_area);
 
-module.exports = router
+module.exports = router;
