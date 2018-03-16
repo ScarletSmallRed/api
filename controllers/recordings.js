d03c148e32cdf40da0252267b44ea6ebac73e2bb
qiang001 <35440329+qiang001@users.noreply.github.com>
2018-03-16 19:47:49 +0800

    lazy

--- controllers/recordings.js
+++ controllers/recordings.js
@@ -13,7 +13,15 @@ exports.recordings_creating_recording = (req, res, next) => {
 };
 
 exports.recordings_get_all = (req, res, next) => {
+  let currentPage =
+    parseInt(req.params.page) > 0 ? parseInt(req.params.page) : 1;
+  let pageSize =
+    parseInt(req.params.pageSize) > 0 ? parseInt(req.params.pageSize) : 10;
+  // 要跳过多少条
+  let skip = (currentPage - 1) * pageSize;
   Recording.find()
+    .limit(pageSize)
+    .skip(skip)
     .populate('operator clerk goods bill')
     .then(doc => {
       console.log(doc);
@@ -25,12 +33,20 @@ exports.recordings_get_all = (req, res, next) => {
 };
 
 exports.recordings_search_recording = (req, res, next) => {
+  let currentPage =
+    parseInt(req.params.page) > 0 ? parseInt(req.params.page) : 1;
+  let pageSize =
+    parseInt(req.params.pageSize) > 0 ? parseInt(req.params.pageSize) : 10;
+  // 要跳过多少条
+  let skip = (currentPage - 1) * pageSize;
   Recording.find({
     time: {
       $gte: new Date(req.body.start),
       $lte: new Date(req.body.end)
     }
   })
+    .limit(pageSize)
+    .skip(skip)
     .populate('operator clerk goods')
     .then(doc => {
       console.log(doc);
--- routes/recordings.js
+++ routes/recordings.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const RecordingController = require('../controllers/recordings');
 
 router.post('/create', RecordingController.recordings_creating_recording);
-router.get('/', RecordingController.recordings_get_all);
-router.post('/search', RecordingController.recordings_search_recording);
+router.get('/:page/:pageSize', RecordingController.recordings_get_all);
+router.post(
+  '/search/:page/:pageSize',
+  RecordingController.recordings_search_recording
+);
 
 module.exports = router;
